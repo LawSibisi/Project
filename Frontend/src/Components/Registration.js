c14ae30c56fec3e557498c9816e02c4565675c72
LawSibisi <167341280+LawSibisi@users.noreply.github.com>
2025-03-19 15:42:00 +0200

    Update Registration.js

--- Frontend/src/Components/Registration.js
+++ Frontend/src/Components/Registration.js
@@ -36,7 +36,7 @@ const RegisterPage = () => {
 
     try {
       const token = localStorage.getItem("token");
-      const response = await axios.post(  "https://project-9upg.onrender.com/api/auth/register",
+      const response = await axios.post(  "https://project-1-ewav.onrender.com/api/auth/register",
         formData,
         {
           headers: { 'Authorization': `Bearer ${token}` },
